b2eb4e161438f55894b3a195dd34902cbd51a2ce
Bradley-Zandgrond <9021118@student.zadkine.nl>
2023-12-06 09:36:08 +0100

    Create for.php

---
+++ for.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$a = 0;
+
+while ($a <= 10) {
+    echo "a: $a<br>";
+
+    $a = $a +2;
+}
+
+?>
+</body>
+</html>
